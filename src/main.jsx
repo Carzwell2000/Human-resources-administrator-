2fd65da4ed008279479cfe61c2800fd1be93c179
carzwell chimanyiwa <carzwellc@gmail.com>
2025-07-26 15:18:36 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { UserProvider } from './Context/UserContext.jsx'; // ✅ Make sure the path is correct
+import { UserProvider } from './Context/UserContext'; // ✅ Make sure the path is correct
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
